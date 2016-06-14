ed2ebcb30b4bfa08f1e70c7284e6e96a6f1506d9
David Armbrust <david.armbrust@gmail.com>
2016-06-14 13:10:42 -0700

    Add remaining questions and ageCheck function

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,18 @@
+def ageCheck(age, year)
+  if (age == 2016-year || age == 2016-year-1)
+    check =  true
+  else
+    check = false
+  end
+  check
+end
 print "What is your name? "
 name = gets
 print "How old are you? "
-age = gets
+age = gets.to_i
 print "What year were you born? "
-birthYear = gets
-print "end"
+birthYear = gets.to_i
+print "Our company serves galic bread. Should we order some for you ?"
+garlicBreadChoice = gets
+print "Would you like to enroll in the company's health insurance? "
+healthInsuranceChoice = gets

9a1077c6c9794326796031117944bd4b936dc2d7
David Armbrust <david.armbrust@gmail.com>
2016-06-14 22:17:14 -0500

    Add check for age and garlic bread preference

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,16 +14,32 @@ age = gets.to_i
 # print "What year were you born? "
 birthYear = gets.to_i
 # print "Our company serves galic bread. Should we order some for you ?"
-garlicBreadChoice = gets
+garlicBreadChoice = gets.chomp!
 # print "Would you like to enroll in the company's health insurance? "
-healthInsuranceChoice = gets
+healthInsuranceChoice = gets.chomp!
 
 vampire_result = "Results inconclusive"
 
-if (ageCheck(age, birthYear) && garlicBreadChoice == "yes")
+
+# If the employee got their age right, and is willing to eat garlic bread or
+# sign up for insurance, the result is “Probably not a vampire.”
+if (ageCheck(age, birthYear) && (garlicBreadChoice == "yes"))
   vampire_result = "Probably not a vampire"
-  puts "passed"
 end
+
+# If the employee got their age wrong, and hates garlic bread or waives
+# insurance, the result is “Probably a vampire.”
+# if (!ageCheck(age, birthYear) && ())
+
+# If the employee got their age wrong, hates garlic bread, and doesn’t want
+# insurance, the result is “Almost certainly a vampire.”
+
+# Even if the employee is an amazing liar otherwise, anyone going by the
+# name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on.
+# In that case, you should print “Definitely a vampire.”
+
+
+# Otherwise, print “Results inconclusive.”
 
 
 puts vampire_result

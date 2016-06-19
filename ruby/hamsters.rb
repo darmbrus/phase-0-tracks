b370989d12d81b6fddf82e96eb59c6a1d9a375ae
David Armbrust <david.armbrust@gmail.com>
2016-06-19 13:47:44 -0500

    Convert input to correct datatype

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,17 +2,21 @@ puts "What is the hamsters name?"
 names = gets.chomp
 
 puts "How would you rate the volume of the hamster? (1-10)"
-volume = gets.chomp
+volume = gets.chomp.to_i
 
 puts "What is the color of the fur?"
 fur_color = gets.chomp
 
 puts "Is the hamster a good candidate for adoption? (yes / no)"
 adoption = gets.chomp
+if (adoption == "yes" || adoption == "y")
+  adoption = true;
+else
+  adoption = false;
+end
 
 puts "What is their estimated age?"
-age = gets.chomp
-
-if (age == "")
+age = gets.chomp.to_i
+if (age == 0)
   age = nil
 end

da8dabefe2bfdc11f4a63346f5ef15775d03e474
David Armbrust <david.armbrust@gmail.com>
2016-06-19 13:55:34 -0500

    Add output statements to show data inputed

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,5 +1,5 @@
 puts "What is the hamsters name?"
-names = gets.chomp
+name = gets.chomp
 
 puts "How would you rate the volume of the hamster? (1-10)"
 volume = gets.chomp.to_i
@@ -20,3 +20,10 @@ age = gets.chomp.to_i
 if (age == 0)
   age = nil
 end
+
+
+puts "The name of the hamster is: " + name
+puts "The volume of the hamster is: " + volume.to_s
+puts "The color of the fur on the hamster is: " + fur_color
+puts "This hamster is a good candidate for adoption: " + adoption.to_s
+puts "The estimated age of the hamster is: " + age.to_s

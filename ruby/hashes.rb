af495b119b7f96f366bd9c51817ef9719cfc5c22
David Armbrust <david.armbrust@gmail.com>
2016-06-23 21:29:23 -0500

    Add sudo code and initialize hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,28 @@
+# data:
+#   Clients name
+#   age/
+#   number of children
+#   decor theme
+#   number of rooms
+#   favorite color
+#   adventurous
+# define a blank hash
+# prompt for all of the data and place in to the hash
+# display the data
+# ask the user if there is anything they would like to change
+# if they enter a key
+#   enter the new data
+# display the update
+
+
+
+client = {
+  name: "",
+  age: 0,
+  number_of_children: 0,
+  decor_theme: "",
+  number_of_rooms: 0,
+  favorite_color: "",
+  adventurous: false,
+}
+puts client

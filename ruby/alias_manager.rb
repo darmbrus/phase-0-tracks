2a7156c37ec5566e84d8eb549ff3a8c1283cb54f
David Armbrust <david.armbrust@gmail.com>
2016-06-26 15:05:58 -0500

    Add methods for finding next letter and swapping names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,18 +9,81 @@
 # if a vowel is found replace it with the next vowel
 # else replace it withe the next letter
 
-def swapLetters(full_name)
+
+# Read the assignment wrong, thought they wanted to swap the first letter of
+# each name.... oh well.
+# def swapLetters(full_name)
+#   split_name = full_name.split(' ')
+#   split_first_name = split_name[0].split('')
+#   split_last_name = split_name[1].split('')
+#
+#   temp = split_last_name[0]
+#   split_last_name[0] = split_first_name[0]
+#   split_first_name[0] = temp
+#
+#   full_name = split_first_name.join('') + ' ' + split_last_name.join('')
+# end
+
+def swapName(full_name)
   split_name = full_name.split(' ')
-  split_first_name = split_name[0].split('')
-  split_last_name = split_name[1].split('')
-
-  temp = split_last_name[0]
-  split_last_name[0] = split_first_name[0]
-  split_first_name[0] = temp
-
-  full_name = split_first_name.join('') + ' ' + split_last_name.join('')
+  temp = split_name[1]
+  split_name[1] = split_name[0]
+  split_name[0] = temp
+  full_name = split_name.join(' ')
 end
 
+def nextLetter(full_name)
+  vowels = 'aeiou'
+  split_letters = full_name.split('')
+  split_letters.map! { |letter|
+    if letter != " "
+      if vowels.index(letter) != nil
+        letter = nextVowel(letter)
+      else
+        letter = nextConsonant(letter)
+      end
+    else
+      # Don't know exactly why this has to be here but I lose my spaces if I
+      # don't do it.
+      letter = letter
+    end
+  }
+  split_letters.join('')
+end
 
-name_test = swapLetters("david Test")
-p name_test
+def nextVowel(letter)
+  is_uppercase = letter == letter.upcase
+  letter.downcase!
+  vowels = 'aeiou'
+  if letter == 'u'
+    letter = 'a'
+  else
+    letter = vowels[vowels.index(letter)+1]
+  end
+  if is_uppercase
+    letter.upcase
+  else
+    letter.downcase
+  end
+end
+
+def nextConsonant(letter)
+  is_uppercase = letter == letter.upcase
+  letter.downcase!
+  consonants = "bcdfghjklmnpqrstvwxyz"
+  if letter == 'z'
+    letter = 'b'
+  else
+    letter = consonants[consonants.index(letter)+1]
+  end
+  if is_uppercase
+    letter.upcase
+  else
+    letter.downcase
+  end
+end
+
+test_name = "Felicia Torres"
+
+name_out = nextLetter(swapName(test_name))
+puts name_out

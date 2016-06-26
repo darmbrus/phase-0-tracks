c044c1dec207b870776f922ae7fef99909c62155
David Armbrust <david.armbrust@gmail.com>
2016-06-26 13:33:49 -0500

    Add pseudocode for alias manager

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# pseudocode:
+# prompt for a name
+# split the name at the space
+# read in the first letter of the last name
+# replace first letter of last name with first letter of the first name
+# replace the first letter of the first name with stored values from last name
+# join the strings back with a space
+# itterate through the string to find vowels
+# if one is found replace it with the next vowel
+
+def swapLetters(full_name)
+  full_name
+end
+
+
+puts swapLetters("david Test")

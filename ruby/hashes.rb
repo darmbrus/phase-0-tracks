c8cc54081d1185034be4418181a4e8455d41e94b
David Armbrust <david.armbrust@gmail.com>
2016-06-26 10:18:09 -0500

    Add checks for input type

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -12,7 +12,7 @@
 # ask the user if there is anything they would like to change
 # if they enter a key
 #   enter the new data
-# display the update
+# display the update_field
 
 
 # Initialize the client hash
@@ -32,7 +32,7 @@ client[:age] = gets.to_i
 puts "How many children do you have: "
 client[:number_of_children] = gets.to_i
 puts "What would you like the theme to be: "
-client[:theme] = gets.chomp
+client[:decor_theme] = gets.chomp
 puts "How many rooms does the space have: "
 client[:number_of_rooms] = gets.to_i
 puts "What is your favorite color: "
@@ -47,10 +47,26 @@ else
 end
 puts client
 puts "Would you like to update any of the fields: "
-update = gets.chomp
-if update != "done"
-  update.prepend(":")
+update_field = gets.chomp
+if update_field != "done"
+  update_field.prepend(":")
   puts "Please enter the value you would like to update it to: "
-  
-  # need to check all possible values of update and convert to the correct type
+  update_value = gets.chomp
+  # need to check all possible values of update_field and convert to the correct type
+  if update_field = ":name" || update_field = ":decor_theme" || update_field = ":favorite_color"
+    client[update_field] = update_value
+  elsif update_field = ":number_of_children" || update_field = "number_of_rooms"
+    client[update_field] = update_value.to_i
+  elsif update_field = ":adventurous_answer"
+    if update_value == "true"
+      client[:adventurous] = true
+    else
+      client[:adventurous] = false
+    end
+  else
+    puts "No value updated"
+  end
+  puts client
+
+
 end

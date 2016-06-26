5e28e91c78449e4ba45455131296c2ced72affb9
David Armbrust <david.armbrust@gmail.com>
2016-06-26 13:36:29 -0500

    Update pseudocode for vowel and consonant changing

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,8 +5,9 @@
 # replace first letter of last name with first letter of the first name
 # replace the first letter of the first name with stored values from last name
 # join the strings back with a space
-# itterate through the string to find vowels
-# if one is found replace it with the next vowel
+# itterate through the string
+# if a vowel is found replace it with the next vowel
+# else replace it withe the next letter
 
 def swapLetters(full_name)
   full_name

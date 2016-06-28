05ed01fdb1f22414dd8e8b3d2f3cb8edba891c6d
David Armbrust <david.armbrust@gmail.com>
2016-06-27 19:46:53 -0500

    Add psuedocode to gps 2.2

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,38 @@
+#pgs2_2
+
+# Method to create a list
+
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+# split up the string
+# Add the user input string to the Hash
+
+
+# set default quantity
+# Set the item number
+
+# print the list to the console [can you use one of your other methods here?]
+# store the Hash inside another variable and print the result variable by the end of program
+# output: [what data type goes here, array or hash?]
+# output the hash we created to the user
+
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: Ask user to input data, change the input to array, call the list_method and pass the parameters to it.
+# output:
+
+# Method to remove an item from the list
+# input: remove an item name and quantity
+# steps:go inside the list_method, hash key and value remove the selected item
+# output: The update list in list_method
+
+# Method to update the quantity of an item
+# input:update the quantit of an item
+# steps: create a loop to ask for more inputs
+# output: call the list_method to print the list
+
+# Method to print a list and make it look pretty
+# input: list of items
+# steps: get the data from user, Go through the methods
+# output: the list

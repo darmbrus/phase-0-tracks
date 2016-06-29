b6d3fc4adba6d1e0117ec7d60fe5065dcb678aa4
David Armbrust <david.armbrust@gmail.com>
2016-06-28 19:38:13 -0500

    Add Release 0 and release 1 from paula

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,41 @@
-class Puppy
-
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-
-end
-
+# class Puppy
+#
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
+#
+#   def speak(num)
+#   	num.times {puts "Woof!"}
+#   end
+#
+#   def roll_over()
+#   	puts "*rolls over*"
+#   end
+#
+#   def dog_years(num)
+#   	age = num * 7
+#   	age
+#   end
+#
+#   def shake_hands()
+#   	puts "*paw out*"
+#   end
+#
+#   def initialize
+#   	puts "Initializing new puppy instance..."
+#   end
+#
+# end
+#
+# spot = Puppy.new
+# buddy = Puppy.new
+#
+#
+#
+# spot.fetch("ball")
+# spot.speak(3)
+# spot.roll_over()
+# age = spot.dog_years(4)
+# puts age
+# spot.shake_hands()

ea2f09b3e96847fc754ce434b8d5b2985bf257da
David Armbrust <david.armbrust@gmail.com>
2016-07-03 16:35:06 -0500

    Clean up and comment code for final

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -26,6 +26,7 @@ class Santa
         @age += 1
     end
 
+    #Method to move a reindeer to the end of the ranking list
     def get_mad_at(nemesis_name)
         @reindeer_ranking.delete(nemesis_name) 
         @reindeer_ranking << nemesis_name
@@ -48,6 +49,7 @@ class Santa
     end
 end
 
+#Initialize default santa parameters
 example_genders = ["agender", "female", "bigender",
     "male", "female", "gender fluid",
     "N/A"]
@@ -56,17 +58,14 @@ example_ethnicities = ["black", "Latino", "white",
     "Mystical Creature (unicorn)", "N/A"]
 number_of_santas = 200
 santas = []
+
 #Create santas and place them in an array
 number_of_santas.times {
     santas << Santa.new(example_genders.sample, example_ethnicities.sample)
 }
-#Set the age of each santa randomly between 0-140 years old
-santas.each { |santa|
-    santa.age = (rand()*140).to_int
-}
 
+#Set the age of each santa randomly between 0-140 years old
+santas.each { |santa| santa.age = (rand()*140).to_int }
 
 #Prints out each of the santa's details
-santas.each { |santa|
-    santa.print_details()
-}
+santas.each { |santa| santa.print_details() }

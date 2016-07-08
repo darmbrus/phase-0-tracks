491fc13d2cf0219805a0fa18254285f02a35b1c4
David Armbrust <david.armbrust@gmail.com>
2016-07-07 23:03:02 -0500

    Add method to change assignment of a task

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -9,8 +9,13 @@
 #   raise priority
 #   assign task
 require 'date'
-class ProjectTask
 
+class ProjectTask
+# Method: initialize
+  # Arguments: start - the start date of the task
+  #            finish - the completion date of the task
+  #            pri - the priority of the task
+  #            assignment - Who the task is assigned to
   def initialize(start, finish, pri, name, assignment)
     parsed_start = start.split('/')
     parsed_end = finish.split('/')
@@ -32,15 +37,19 @@ class ProjectTask
     else
       @priority = pri
     end
-
     @task_name = name
     @assigned_to = assignment
   end
-
+# Method: get_duration
+  # Arguments: none
+  # Will return the number of days a task is scheduled to take
   def get_duration()
     puts "The task will take #{(@date_end - @date_start).to_i} days"
   end
-
+# Method: raise_priority
+  # Arguments: none
+  # Will raise the priority of the task by one with in the limits of
+  # 0 and 10
   def raise_priority()
     if @priority < 10
       @priority += 1
@@ -48,7 +57,15 @@ class ProjectTask
       @priority
     end
   end
-
+# Method: assign_to
+  # Arguments: new_owner - the new owner to be assigned to the task
+  # Will change the owner to the value passed in
+  def assign_to(new_owner)
+    @assigned_to = new_owner
+  end
+# Method: print_task
+  # Arguments: none
+  # Will print out the details of the task
   def print_task()
     puts @task_name
     puts "  Assigned to: #{@assigned_to}"
@@ -66,4 +83,6 @@ end
 
 task = ProjectTask.new("7/7/2016", "7/20/2016", "3", "homework", "David")
 task.get_duration
+task.print_task
+task.assign_to("bob")
 task.print_task

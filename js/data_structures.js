67bbde1831aff31bbda54aeb11431c3970bec8eb
David Armbrust <david.armbrust@gmail.com>
2016-07-14 20:10:45 -0500

    Add cars function

--- js/data_structures.js
+++ js/data_structures.js
@@ -21,3 +21,22 @@ if (names.length == colors.length) {
 console.log(horses);
 
 
+function Car(color, year, model) {
+  this.color = color;
+  this.year = year;
+  this.model = model;
+
+  this.drive = function() { console.log('VROOOOM'); }
+
+}
+
+
+console.log("Creating new car: ")
+var car1 = new Car("blue", 1974, "geo metro");
+
+car1.drive();
+
+var car2 = new Car("red", 2010, "Camaro");
+
+console.log("You just won a " + car2.color + " " + car2.year
+            + " " + car2.model + ".");

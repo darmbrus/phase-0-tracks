9021c20f605de8d934d36e3ead3cb55a92fa7bda
David Armbrust <david.armbrust@gmail.com>
2016-07-14 19:34:59 -0500

    Add 2 arrays for names and colors

---
+++ js/data_structures.js
@@ -0,0 +1,19 @@
+// Create two arrays
+
+var colors = ["red", "green", "blue", "purple"];
+
+var names = ["James", "Lary", "Matilda", "Sue"];
+
+
+colors.push("orange");
+
+names.push("Gavin");
+
+console.log(colors);
+console.log(names);
+
+for(var i = 0; i < names.length; i++) {
+  
+
+
+}

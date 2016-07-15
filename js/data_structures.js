83241a5e66aa77931f2887d87a43636d31006448
David Armbrust <david.armbrust@gmail.com>
2016-07-14 19:42:24 -0500

    Add horse object

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,12 +8,16 @@ var names = ["James", "Lary", "Matilda", "Sue"];
 colors.push("orange");
 
 names.push("Gavin");
-
+// names.push("Steve");
 console.log(colors);
 console.log(names);
 
-for(var i = 0; i < names.length; i++) {
-  
-
-
+horses = {};
+if (names.length == colors.length) {
+  for(var i = 0; i < names.length; i++) {
+    horses[names[i]] = colors[i];
+  }
 }
+console.log(horses);
+
+

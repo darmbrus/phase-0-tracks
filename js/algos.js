485402bc008ada328af1fd03ca4c505bfbf6261f
David Armbrust <david.armbrust@gmail.com>
2016-07-17 15:13:05 -0500

    Add random words function

--- js/algos.js
+++ js/algos.js
@@ -12,10 +12,10 @@ function longest(array) {
   }
 }
 
-var testArray = ["long", "longer", "longest"];
-console.log(longest(testArray));
-testArray = ["12345", "123456", "1234"];
-console.log(longest(testArray));
+// var testArray = ["long", "longer", "longest"];
+// console.log(longest(testArray));
+// testArray = ["12345", "123456", "1234"];
+// console.log(longest(testArray));
 
 // Write a function that takes two objects
 // and check to see if they share a key 
@@ -28,19 +28,51 @@ function testMatch(x,y) {
 }
 
 
-var one = {name: "John", age: 54};
-var two = {name: "Peter", age: 54};
+// var one = {name: "John", age: 54};
+// var two = {name: "Peter", age: 54};
+// 
+// if (testMatch(one, two)) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+// one = {name: "John", age: 54};
+// two = {name: "Peter", age: 53};
+// 
+// if (testMatch(one, two)) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
 
-if (testMatch(one, two)) {
-  console.log("yes");
-} else {
-  console.log("no");
+// Write a function that will generate an array
+// of a defined length of random words
+function randomWords(arrayLength){
+  var stringLength = 0;
+  var stringArray = [];
+  var currentWord = "";
+  for (var i = 0; i < arrayLength; i++) {
+    stringArray.push(randomString(Math.random() * 10));
+  }
+  return(stringArray);
 }
-one = {name: "John", age: 54};
-two = {name: "Peter", age: 53};
 
-if (testMatch(one, two)) {
-  console.log("yes");
-} else {
-  console.log("no");
+function randomChar(){
+  return(String.fromCharCode(97 + 26 * Math.random()));
 }
+
+function randomString(stringLength) {
+  var word = "";
+  for (var i = 0; i < stringLength; i++) {
+    word += randomChar();
+  }
+  return(word);
+}
+// console.log(randomWords(3));
+var wordArray = [];
+for(var i = 0; i < 10; i++) {
+  wordArray = randomWords(Math.random() * 10);
+  console.log(wordArray);
+  console.log(longest(wordArray));
+}
+

33ae14dbe5ba68973b4052325778e0d2fa2c3f9f
David Armbrust <david.armbrust@gmail.com>
2016-07-18 22:43:52 -0500

    Add build db and projects table

--- final/database.rb
+++ final/database.rb
@@ -2,7 +2,15 @@ require 'sqlite3'
 
 module DB_actions
   def self.init_database
-    db = SQLite3::Database.new('test')
+    db = SQLite3::Database.new('projects.db')
+    sql = <<-SQL
+      CREATE TABLE Projects (
+        projId INTEGER PRIMARY KEY,
+        projName VARCHAR(255),
+        projValue DOUBLE(9,2)
+      );
+    SQL
+    db.execute(sql)
   end
 
 
--- final/projects.rb
+++ final/projects.rb
@@ -1,4 +1,5 @@
 require_relative 'database'
+require 'sqlite3'
 
 DB_actions.init_database
 

fd05b1c071f553786cb5c1bbd50925b898c2c715
David Armbrust <david.armbrust@gmail.com>
2016-07-19 12:51:42 -0500

    Create add task method

--- final/database.rb
+++ final/database.rb
@@ -14,13 +14,21 @@ module DB_actions
     SQL
     db.execute(sql)
     sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS Engineers (
+        engId INTEGER PRIMARY KEY,
+        engName VARCHAR(255)
+      );
+    SQL
+    db.execute(sql)
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS Tasks (
         taskId INTEGER PRIMARY KEY,
         taskName VARCHAR(255),
         taskComplete BOOLEAN,
-        taskAssignment VARCHAR(255),
         projId INTEGER,
-        FOREIGN KEY (projID) REFERENCES Projects(projID)
+        engId INTEGER,
+        FOREIGN KEY (projId) REFERENCES Projects(projId),
+        FOREIGN KEY (engId) REFERENCES Engineers(engId)
       );
     SQL
     db.execute(sql)
@@ -30,12 +38,32 @@ module DB_actions
   # Method to create a new project entry, assumes project is not complete
   # Input:
   #   db - the database to update
-  #   name - the project names
-  #   
+  #   name - the project name
   def self.add_project(db, name)
     db.execute("INSERT INTO Projects (projName, projComplete) VALUES (?, ?)", 
                [name, "false"])
   end
 
+  # Method to create a new engineer
+  # Input:
+  #   db - the database to update
+  #   name - the engineer name
+  def self.add_engineer(db, name)
+    db.execute("INSERT INTO Engineers (engName) VALUES (?)", 
+               [name])
+  end
 
+  # Method to create a new task and assign it to a project and optional 
+  # engineer
+  # Input:
+  #   db - the database to update
+  #   name - the task name
+  def self.add_task(db, name, projId, engId)
+    db.execute("INSERT INTO Tasks (
+               taskName,
+               projId,
+               engId
+              ) VALUES (?, ?, ?)", 
+               [name, projId,  engId])
+  end
 end
--- final/projects.rb
+++ final/projects.rb
@@ -2,5 +2,9 @@ require_relative 'database'
 require 'sqlite3'
 
 db = DB_actions.init_database
-
+puts "creating project"
 DB_actions.add_project(db, "test")
+puts "creating engineer"
+DB_actions.add_engineer(db, "David")
+puts "creating task"
+DB_actions.add_task(db, "test task 1", 1, 1)

e05d86047541fe75c1d685254b62cae1e233a3c6
David Armbrust <david.armbrust@gmail.com>
2016-07-24 10:33:23 -0500

    Add database and engineer submenus

--- final/interface_project.rb
+++ final/interface_project.rb
@@ -10,27 +10,62 @@ def print_main_menu()
   puts "5  - Add project"
   puts "6  - Add Task"
 
+  print "Selection: "
 end
 
+def print_database_submenu()
+  puts "0  - Return to main menu"
+  puts "1  - Connect to a database"
+  print "Selection: "
+end
+
+def print_engineer_submenu()
+  puts "0  - Return to main menu"
+  puts "1  - List engineers"
+  puts "2  - Add an engineer"
+
+  print "Selection: "
+end
 
 db = nil
 system "clear"
 choice = 0
+print_main_menu()
+choice = gets.chomp.to_i
  
-loop do
-  print "Selection: "
+begin
+  case choice
+  when 1
+    begin
+      puts "sub 1"
+      print_database_submenu()
+      choice = gets.chomp.to_i
+      case choice
+      when -1
+         system "clear"
+         print_main_menu()
+      when 1
+        system "clear"
+        print_main_menu()
+        db = DB_actions.init_database()
+      end
+    end until choice == 0
+
+  when 2
+    begin
+      puts "sub 2"
+      print_engineer_submenu()
+      choice = gets.chomp.to_i
+      case choice
+      when 1
+        DB_actions.print_engineers(db)
+      when 2
+        name = gets.chomp
+        DB_actions.add_engineer(db, name)
+      end
+    end until choice == 0
+  end
+
   print_main_menu()
   choice = gets.chomp.to_i
-  puts choice
-  case choice
-  when -1
-    system "clear"
-  when 1
-    system "clear"
-    db = DB_actions.init_database()
-  when 2
-    system "clear"
-    DB_actions.print_projects(db)
-  end
-  break if choice == 0
-end  
+end until choice == 0 
